9b9b3e4245ff6c7b562d8152fb9d1fe9ff8aaf7f
mrimavicius <mantas.rimaviciuss@gmail.com>
2022-05-04 14:47:08 +0300

    v.1 hardcoded

---
+++ script.js
@@ -0,0 +1,84 @@
+"use strict"
+
+// v.1.0
+var buttons = document.getElementsByTagName("button")
+var answerWas = document.querySelectorAll("p")[0]
+var result = document.querySelectorAll("p")[1]
+var won = document.querySelectorAll("span")[0]
+var tied = document.querySelectorAll("span")[1]
+var lost = document.querySelectorAll("span")[2]
+var winCount = 0
+var tieCount = 0
+var loseCount = 0
+
+for(var x of buttons){
+    x.addEventListener("click", function(){
+        var random = Math.floor(Math.random() * 3)        
+        var options = ["Rock", "Paper", "Scissors"]
+        var answer = options[random]
+
+        console.log(answer)
+        console.log(this.innerHTML)
+        
+        // var answerRock = answerWas.innerHTML = (`Answer was ROCK <i class="fa-solid fa-hand-back-fist"></i>`)
+        // var answerPaper = answerWas.innerHTML = (`Answer was PAPER <i class="fa-solid fa-hand-back-fist"></i>`)
+        var answerScissors = answerWas.innerHTML = (`Answer was SCISSORS <i class="fa-solid fa-hand-back-fist"></i>`)
+
+        if(answer == "Rock" && this.innerHTML == 'Rock <i class="fa-solid fa-hand-back-fist"></i>'){
+            var answerRock = answerWas.innerHTML = (`Answer was ROCK <i class="fa-solid fa-hand-back-fist"></i>`)
+            var youTied = result.innerText = (`It's a Tie!`)
+            tieCount++
+            tied.innerText = (`${tieCount}`)  
+        }
+        if(answer == "Paper" && this.innerHTML == 'Rock <i class="fa-solid fa-hand-back-fist"></i>'){
+            var answerPaper = answerWas.innerHTML = (`Answer was PAPER <i class="fa-solid fa-hand-back-fist"></i>`)
+            var youLost = result.innerText = (`You Lost!`)
+            loseCount++
+            lost.innerText = (`${loseCount}`)
+        }
+        if(answer == "Scissors" && this.innerHTML == 'Rock <i class="fa-solid fa-hand-back-fist"></i>'){
+            var answerScissors = answerWas.innerHTML = (`Answer was SCISSORS <i class="fa-solid fa-hand-back-fist"></i>`)
+            var youWon = result.innerText = (`You Won!`)
+            winCount++,
+            won.innerText = (`${winCount}`)
+        }
+
+        if(answer == "Rock" && this.innerHTML == 'Paper <i class="fa-solid fa-hand"></i>'){
+            var answerRock = answerWas.innerHTML = (`Answer was ROCK <i class="fa-solid fa-hand-back-fist"></i>`)
+            var youWon = result.innerText = (`You Won!`)
+            winCount++,
+            won.innerText = (`${winCount}`)
+        }
+        if(answer == "Paper" && this.innerHTML == 'Paper <i class="fa-solid fa-hand"></i>'){
+            var answerPaper = answerWas.innerHTML = (`Answer was PAPER <i class="fa-solid fa-hand-back-fist"></i>`)
+            var youTied = result.innerText = (`It's a Tie!`)
+            tieCount++
+            tied.innerText = (`${tieCount}`)  
+        }
+        if(answer == "Scissors" && this.innerHTML == 'Paper <i class="fa-solid fa-hand"></i>'){
+            var answerScissors = answerWas.innerHTML = (`Answer was SCISSORS <i class="fa-solid fa-hand-back-fist"></i>`)
+            var youLost = result.innerText = (`You Lost!`)
+            loseCount++
+            lost.innerText = (`${loseCount}`)
+        }
+
+        if(answer == "Rock" && this.innerHTML == 'Scissors <i class="fa-solid fa-hand-scissors"></i>'){
+            var answerRock = answerWas.innerHTML = (`Answer was ROCK <i class="fa-solid fa-hand-back-fist"></i>`)
+            var youLost = result.innerText = (`You Lost!`)
+            loseCount++
+            lost.innerText = (`${loseCount}`)
+        }
+        if(answer == "Paper" && this.innerHTML == 'Scissors <i class="fa-solid fa-hand-scissors"></i>'){
+            var answerPaper = answerWas.innerHTML = (`Answer was PAPER <i class="fa-solid fa-hand-back-fist"></i>`)
+            var youWon = result.innerText = (`You Won!`)
+            winCount++,
+            won.innerText = (`${winCount}`)
+        }
+        if(answer == "Scissors" && this.innerHTML == 'Scissors <i class="fa-solid fa-hand-scissors"></i>'){
+            var answerScissors = answerWas.innerHTML = (`Answer was SCISSORS <i class="fa-solid fa-hand-back-fist"></i>`)
+            var youTied = result.innerText = (`It's a Tie!`)
+            tieCount++
+            tied.innerText = (`${tieCount}`)  
+        }
+    })
+}
