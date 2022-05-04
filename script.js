304cf086ccdfe6bfdb03838dae7c270e55836c3c
mrimavicius <mantas.rimaviciuss@gmail.com>
2022-05-04 17:44:02 +0300

    minor style fixes

--- script.js
+++ script.js
@@ -21,21 +21,21 @@ for(var x of buttons){
         // console.log(this.innerHTML)
         
         if(answer == "Rock" && this.innerHTML == 'Rock <i class="fa-solid fa-hand-back-fist"></i>'){
-            var answerRock = answerWas.innerHTML = (`Answer was ROCK <i class="fa-solid fa-hand-back-fist"></i>`)
+            var answerRock = answerWas.innerHTML = (`Opponent chose ROCK <i class="fa-solid fa-hand-back-fist"></i>`)
             var youTied = result.innerText = (`It's a Tie!`)
             youTied = result.style.color = "grey"
             tieCount++
             tied.innerText = (`${tieCount}`)  
         }
         if(answer == "Paper" && this.innerHTML == 'Rock <i class="fa-solid fa-hand-back-fist"></i>'){
-            var answerPaper = answerWas.innerHTML = (`Answer was PAPER <i class="fa-solid fa-hand-back-fist"></i>`)
+            var answerPaper = answerWas.innerHTML = (`Opponent chose PAPER <i class="fa-solid fa-hand-back-fist"></i>`)
             var youLost = result.innerText = (`You Lost!`)
             youLost = result.style.color = "red"
             loseCount++
             lost.innerText = (`${loseCount}`)
         }
         if(answer == "Scissors" && this.innerHTML == 'Rock <i class="fa-solid fa-hand-back-fist"></i>'){
-            var answerScissors = answerWas.innerHTML = (`Answer was SCISSORS <i class="fa-solid fa-hand-back-fist"></i>`)
+            var answerScissors = answerWas.innerHTML = (`Opponent chose SCISSORS <i class="fa-solid fa-hand-back-fist"></i>`)
             var youWon = result.innerText = (`You Won!`)
             youWon = result.style.color = "green"
             winCount++,
@@ -43,21 +43,21 @@ for(var x of buttons){
         }
 
         if(answer == "Rock" && this.innerHTML == 'Paper <i class="fa-solid fa-hand"></i>'){
-            var answerRock = answerWas.innerHTML = (`Answer was ROCK <i class="fa-solid fa-hand-back-fist"></i>`)
+            var answerRock = answerWas.innerHTML = (`Opponent chose ROCK <i class="fa-solid fa-hand-back-fist"></i>`)
             var youWon = result.innerText = (`You Won!`)
             youWon = result.style.color = "green"
             winCount++,
             won.innerText = (`${winCount}`)
         }
         if(answer == "Paper" && this.innerHTML == 'Paper <i class="fa-solid fa-hand"></i>'){
-            var answerPaper = answerWas.innerHTML = (`Answer was PAPER <i class="fa-solid fa-hand-back-fist"></i>`)
+            var answerPaper = answerWas.innerHTML = (`Opponent chose PAPER <i class="fa-solid fa-hand-back-fist"></i>`)
             var youTied = result.innerText = (`It's a Tie!`)
             youTied = result.style.color = "grey"
             tieCount++
             tied.innerText = (`${tieCount}`)  
         }
         if(answer == "Scissors" && this.innerHTML == 'Paper <i class="fa-solid fa-hand"></i>'){
-            var answerScissors = answerWas.innerHTML = (`Answer was SCISSORS <i class="fa-solid fa-hand-back-fist"></i>`)
+            var answerScissors = answerWas.innerHTML = (`Opponent chose SCISSORS <i class="fa-solid fa-hand-back-fist"></i>`)
             var youLost = result.innerText = (`You Lost!`)
             youLost = result.style.color = "red"
             loseCount++
@@ -65,21 +65,21 @@ for(var x of buttons){
         }
 
         if(answer == "Rock" && this.innerHTML == 'Scissors <i class="fa-solid fa-hand-scissors"></i>'){
-            var answerRock = answerWas.innerHTML = (`Answer was ROCK <i class="fa-solid fa-hand-back-fist"></i>`)
+            var answerRock = answerWas.innerHTML = (`Opponent chose ROCK <i class="fa-solid fa-hand-back-fist"></i>`)
             var youLost = result.innerText = (`You Lost!`)
             youLost = result.style.color = "red"
             loseCount++
             lost.innerText = (`${loseCount}`)
         }
         if(answer == "Paper" && this.innerHTML == 'Scissors <i class="fa-solid fa-hand-scissors"></i>'){
-            var answerPaper = answerWas.innerHTML = (`Answer was PAPER <i class="fa-solid fa-hand-back-fist"></i>`)
+            var answerPaper = answerWas.innerHTML = (`Opponent chose PAPER <i class="fa-solid fa-hand-back-fist"></i>`)
             var youWon = result.innerText = (`You Won!`)
             youWon = result.style.color = "green"
             winCount++,
             won.innerText = (`${winCount}`)
         }
         if(answer == "Scissors" && this.innerHTML == 'Scissors <i class="fa-solid fa-hand-scissors"></i>'){
-            var answerScissors = answerWas.innerHTML = (`Answer was SCISSORS <i class="fa-solid fa-hand-back-fist"></i>`)
+            var answerScissors = answerWas.innerHTML = (`Opponent chose SCISSORS <i class="fa-solid fa-hand-back-fist"></i>`)
             var youTied = result.innerText = (`It's a Tie!`)
             youTied = result.style.color = "grey"
             tieCount++
