a15c854dec92efb0f2ae7d19648a281afeceb9ad
mrimavicius <mantas.rimaviciuss@gmail.com>
2022-05-31 10:43:39 +0300

    script optimisation

--- script.js
+++ script.js
@@ -7,83 +7,54 @@ var result = document.querySelectorAll("p")[1]
 var won = document.querySelectorAll("span")[0]
 var tied = document.querySelectorAll("span")[1]
 var lost = document.querySelectorAll("span")[2]
-var winCount = 0
-var tieCount = 0
-var loseCount = 0
 
 for(var x of buttons){
     x.addEventListener("click", function(){
         var random = Math.floor(Math.random() * 3)        
-        var options = ["Rock", "Paper", "Scissors"]
+        var options = [
+            `ROCK <i class="fa-solid fa-hand-back-fist"></i>`,
+            `PAPER <i class="fa-solid fa-hand"></i>`,
+            `SCISSORS <i class="fa-solid fa-hand-scissors"></i>`]
         var answer = options[random]
-
-        // console.log(answer)
-        // console.log(this.innerHTML)
         
-        if(answer == "Rock" && this.innerHTML == 'Rock <i class="fa-solid fa-hand-back-fist"></i>'){
-            var answerRock = answerWas.innerHTML = (`Opponent chose ROCK <i class="fa-solid fa-hand-back-fist"></i>`)
-            var youTied = result.innerText = (`It's a Tie!`)
-            youTied = result.style.color = "grey"
-            tieCount++
-            tied.innerText = (`${tieCount}`)  
+        if(
+            (answer == options[0] && this.textContent == 'Rock ') ||
+            (answer == options[1] && this.textContent == 'Paper ') ||
+            (answer == options[2] && this.textContent == 'Scissors ')){
+                tieTie(answer)
         }
-        if(answer == "Paper" && this.innerHTML == 'Rock <i class="fa-solid fa-hand-back-fist"></i>'){
-            var answerPaper = answerWas.innerHTML = (`Opponent chose PAPER <i class="fa-solid fa-hand-back-fist"></i>`)
-            var youLost = result.innerText = (`You Lost!`)
-            youLost = result.style.color = "red"
-            loseCount++
-            lost.innerText = (`${loseCount}`)
+        if(
+            (answer == options[1] && this.textContent == 'Rock ') ||
+            (answer == options[2] && this.textContent == 'Paper ') ||
+            (answer == options[0] && this.textContent == 'Scissors ')){
+                loseLose(answer)
         }
-        if(answer == "Scissors" && this.innerHTML == 'Rock <i class="fa-solid fa-hand-back-fist"></i>'){
-            var answerScissors = answerWas.innerHTML = (`Opponent chose SCISSORS <i class="fa-solid fa-hand-back-fist"></i>`)
-            var youWon = result.innerText = (`You Won!`)
-            youWon = result.style.color = "green"
-            winCount++,
-            won.innerText = (`${winCount}`)
-        }
-
-        if(answer == "Rock" && this.innerHTML == 'Paper <i class="fa-solid fa-hand"></i>'){
-            var answerRock = answerWas.innerHTML = (`Opponent chose ROCK <i class="fa-solid fa-hand-back-fist"></i>`)
-            var youWon = result.innerText = (`You Won!`)
-            youWon = result.style.color = "green"
-            winCount++,
-            won.innerText = (`${winCount}`)
-        }
-        if(answer == "Paper" && this.innerHTML == 'Paper <i class="fa-solid fa-hand"></i>'){
-            var answerPaper = answerWas.innerHTML = (`Opponent chose PAPER <i class="fa-solid fa-hand-back-fist"></i>`)
-            var youTied = result.innerText = (`It's a Tie!`)
-            youTied = result.style.color = "grey"
-            tieCount++
-            tied.innerText = (`${tieCount}`)  
-        }
-        if(answer == "Scissors" && this.innerHTML == 'Paper <i class="fa-solid fa-hand"></i>'){
-            var answerScissors = answerWas.innerHTML = (`Opponent chose SCISSORS <i class="fa-solid fa-hand-back-fist"></i>`)
-            var youLost = result.innerText = (`You Lost!`)
-            youLost = result.style.color = "red"
-            loseCount++
-            lost.innerText = (`${loseCount}`)
-        }
-
-        if(answer == "Rock" && this.innerHTML == 'Scissors <i class="fa-solid fa-hand-scissors"></i>'){
-            var answerRock = answerWas.innerHTML = (`Opponent chose ROCK <i class="fa-solid fa-hand-back-fist"></i>`)
-            var youLost = result.innerText = (`You Lost!`)
-            youLost = result.style.color = "red"
-            loseCount++
-            lost.innerText = (`${loseCount}`)
-        }
-        if(answer == "Paper" && this.innerHTML == 'Scissors <i class="fa-solid fa-hand-scissors"></i>'){
-            var answerPaper = answerWas.innerHTML = (`Opponent chose PAPER <i class="fa-solid fa-hand-back-fist"></i>`)
-            var youWon = result.innerText = (`You Won!`)
-            youWon = result.style.color = "green"
-            winCount++,
-            won.innerText = (`${winCount}`)
-        }
-        if(answer == "Scissors" && this.innerHTML == 'Scissors <i class="fa-solid fa-hand-scissors"></i>'){
-            var answerScissors = answerWas.innerHTML = (`Opponent chose SCISSORS <i class="fa-solid fa-hand-back-fist"></i>`)
-            var youTied = result.innerText = (`It's a Tie!`)
-            youTied = result.style.color = "grey"
-            tieCount++
-            tied.innerText = (`${tieCount}`)  
+        if(
+            (answer == options[2] && this.textContent == 'Rock ') ||
+            (answer == options[0] && this.textContent == 'Paper ') ||
+            (answer == options[1] && this.textContent == 'Scissors ')){
+                winWin(answer)
         }
     })
+}
+
+function winWin(answer) {
+    answerWas.innerHTML = (`Opponent chose ${answer}`)
+    result.innerText = (`You Won!`)
+    result.style.color = "green"
+    won.textContent++
+}
+
+function loseLose(answer) {
+    answerWas.innerHTML = (`Opponent chose ${answer}`)
+    result.innerText = (`You Lost!`)
+    result.style.color = "red"
+    lost.textContent++
+}
+
+function tieTie(answer) {
+    answerWas.innerHTML = (`Opponent chose ${answer}`)
+    result.innerText = (`It's a Tie!`)
+    result.style.color = "grey"
+    tied.textContent++
 }
