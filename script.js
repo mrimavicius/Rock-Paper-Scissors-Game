c7bb90092fb10bf5f782273537b77e235386a25e
mrimavicius <mantas.rimaviciuss@gmail.com>
2022-05-31 15:39:29 +0300

    init v.2.0

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 "use strict"
 
-// v.1.0
+// v.2.0
 var buttons = document.getElementsByTagName("button")
-var answerWas = document.querySelectorAll("p")[0]
-var result = document.querySelectorAll("p")[1]
+var roll = document.querySelector(".roll")
+var answerWas = document.querySelector(".answerWas")
+var result = document.querySelector(".result")
 var won = document.querySelectorAll("span")[0]
 var tied = document.querySelectorAll("span")[1]
 var lost = document.querySelectorAll("span")[2]
 
+// Add click to all options
 for(var x of buttons){
     x.addEventListener("click", function(){
+        // Clear results
+        result.innerText = ""
+
+        // Generate random option for opponent
         var random = Math.floor(Math.random() * 3)        
         var options = [
             `ROCK <i class="fa-solid fa-hand-back-fist"></i>`,
@@ -17,43 +23,71 @@ for(var x of buttons){
             `SCISSORS <i class="fa-solid fa-hand-scissors"></i>`]
         var answer = options[random]
         
-        if(
-            (answer == options[0] && this.textContent == 'Rock ') ||
-            (answer == options[1] && this.textContent == 'Paper ') ||
-            (answer == options[2] && this.textContent == 'Scissors ')){
-                tieTie(answer)
+        // Start animation on button click
+        roll.classList.add("animation")
+        roll.innerHTML = `
+        <p><i class="fa-regular fa-hand-point-down"></i></p>
+        <p></p>
+        <p><i class="fa-regular fa-hand-point-down"></i></p>
+        `
+
+        // Disable buttons for animation duration
+        for(var x of buttons){
+            x.disabled = true
         }
-        if(
-            (answer == options[1] && this.textContent == 'Rock ') ||
-            (answer == options[2] && this.textContent == 'Paper ') ||
-            (answer == options[0] && this.textContent == 'Scissors ')){
-                loseLose(answer)
-        }
-        if(
-            (answer == options[2] && this.textContent == 'Rock ') ||
-            (answer == options[0] && this.textContent == 'Paper ') ||
-            (answer == options[1] && this.textContent == 'Scissors ')){
-                winWin(answer)
-        }
+
+        setTimeout(() => {
+            for(var x of buttons){
+                x.disabled = false
+            }  
+        }, 1000);
+
+        // Show results after animation ends
+        setTimeout(() => {
+            roll.innerHTML = `
+            <p>${this.innerHTML}</p>
+            <p>VS</p>
+            <p>${answer}</p>
+            `
+
+            if(
+                (answer == options[0] && this.innerText == 'ROCK ') ||
+                (answer == options[1] && this.innerText == 'PAPER ') ||
+                (answer == options[2] && this.innerText == 'SCISSORS ')){
+                    tieTie()
+            }
+            if(
+                (answer == options[1] && this.innerText == 'ROCK ') ||
+                (answer == options[2] && this.innerText == 'PAPER ') ||
+                (answer == options[0] && this.innerText == 'SCISSORS ')){
+                    loseLose()
+            }
+            if(
+                (answer == options[2] && this.innerText == 'ROCK ') ||
+                (answer == options[0] && this.innerText == 'PAPER ') ||
+                (answer == options[1] && this.innerText == 'SCISSORS ')){
+                    winWin()
+            }
+
+            // Remove animation when results are shown
+            roll.classList.remove("animation")
+        }, 1000)  
     })
 }
 
-function winWin(answer) {
-    answerWas.innerHTML = (`Opponent chose ${answer}`)
+function winWin() {
     result.innerText = (`You Won!`)
     result.style.color = "green"
     won.textContent++
 }
 
-function loseLose(answer) {
-    answerWas.innerHTML = (`Opponent chose ${answer}`)
+function loseLose() {
     result.innerText = (`You Lost!`)
     result.style.color = "red"
     lost.textContent++
 }
 
-function tieTie(answer) {
-    answerWas.innerHTML = (`Opponent chose ${answer}`)
+function tieTie() {
     result.innerText = (`It's a Tie!`)
     result.style.color = "grey"
     tied.textContent++
